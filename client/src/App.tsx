6b02a32a38f4c46355ac75419734d9f7ec248456
jplp <jp.paiao03@gmail.com>
2023-09-20 18:56:51 -0300

    style: Header do chats adicionado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { SideBarChats } from "./components/sideBarChats"
 import { Chats } from "./components/chat"
 import { Channels } from "./components/channels"
+import { useState } from "react"
 
 function App() {
+  const [chatSelect, setChatSelect] = useState("geral")
+  
   return (
-    <div className="flex">
+    <div className="flex w-screen h-screen">
       <SideBarChats />
-      <div className="flex">
-        <Channels />
-        <Chats />
+      <div className="flex w-full">
+        <Channels channelSelect={chatSelect} setChannelSelect={setChatSelect} />
+        <Chats channel={chatSelect} />
       </div>
     </div>
   )
