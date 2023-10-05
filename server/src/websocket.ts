335f7cc70a6c2d37d59f165fb13b401232db0ed8
jplp <jp.paiao03@gmail.com>
2023-10-05 20:04:53 -0300

    feat: Adicionado as salas e os usuários as salas

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -2,16 +2,70 @@ import { io } from "./http";
 
 interface Message {
   id: string,
-  name: string,
+  username: string,
   img: string,
   text: string
+}
+
+interface RoomUser {
+  username: string,
+  socket_id: string
+}
+
+interface Rooms {
+  [key: string]: {
+    users: RoomUser[],
+    messagens: Message[]
+  }
+}
+
+const users: RoomUser[] = []
+const rooms: Rooms = {}
+
+// const r = {
+//   geral: {
+//     users: [],
+//     messagens: []
+//   },
+//   git: {
+//     users: [],
+//     messagens: []
+//   },
+// }
+
+// function delUserRoom(room: string, id: string) {
+//   Object.keys(rooms).map((r) => {
+//     console.log(rooms[r])
+    
+//     if (r != room) {
+//       console.log(" ")
+//       console.log(r)
+      
+//       // const remove = rooms[r].users.filter(user => user.socket_id !== id)
+//       // console.log(remove)
+//     }
+//   })
+// }
+
+function setUserRoom(room: string, id: string) {
+  const userInRoom = users.filter(user => id === user.socket_id)
+  const userId = rooms[room].users.find(user => user.socket_id === userInRoom[0].socket_id)
+
+  if (!userId) rooms[room].users.push(userInRoom[0])
+}
+
+function setRoom(newRoom: string) {
+  rooms[newRoom] = {
+    users: [],
+    messagens: []
+  }
 }
 
 io.on('connection', socket => {
   socket.on('msg', (msg: Message): void => {
     const newMessage: Message = {
       id: socket.data.id,
-      name: socket.data.username,
+      username: socket.data.username,
       img: msg.img,
       text: msg.text
     }
@@ -19,9 +73,24 @@ io.on('connection', socket => {
     io.emit('msgs', newMessage)
   })
   
+  socket.on('select_room', (data) => {
+    const dataRoom: string = data.room
+
+    socket.join(dataRoom)
+    if (!rooms.hasOwnProperty(dataRoom)) setRoom(dataRoom)
+    // delUserRoom(dataRoom, data.socket_id)
+    setUserRoom(dataRoom, data.socket_id)
+  })
 
   socket.on('set_username', (username: string) => {
     socket.data.username = username
     socket.data.id = socket.id
+
+    io.emit("user_id", socket.data.id)
+
+    users.push({
+      username: socket.data.username,
+      socket_id: socket.data.id
+    })
   })
 })  
