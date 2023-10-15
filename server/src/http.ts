535aff19b60421fc8b9e29d1c10b6fca2654234e
jplp <jp.paiao03@gmail.com>
2023-10-14 21:02:22 -0300

    refactor: Http cors

--- server/src/http.ts
+++ server/src/http.ts
@@ -4,6 +4,11 @@ import { Server } from "socket.io"
 
 const app = express()
 const serverHttp = http.createServer(app)
-const io = new Server(serverHttp, {cors :{origin: "https://master--luminous-cajeta-a0731b.netlify.app/"}})
+const io = new Server(serverHttp, {
+	cors :{
+		origin: "https://master--luminous-cajeta-a0731b.netlify.app/",
+    credentials: true
+	}
+})
 
 export { serverHttp, io, app }
