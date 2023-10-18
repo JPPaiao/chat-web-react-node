285421f8ad4051164f59b254be9e36aae92daacf
jplp <jp.paiao03@gmail.com>
2023-10-18 17:33:59 -0300

    refactor: cors com socket, server e client

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client"
 
-const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:3000", {
-	withCredentials: true,
-})
+const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:3000")
 
 export { socket }
--- server/src/http.ts
+++ server/src/http.ts
@@ -4,9 +4,9 @@ import { Server } from "socket.io"
 
 const app = express()
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://master--luminous-cajeta-a0731b.netlify.app/')
+    res.header('Access-Control-Allow-Origin', 'https://master--luminous-cajeta-a0731b.netlify.app')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type')
-    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Credentials', 'false')
     next()
 })
 
@@ -14,8 +14,8 @@ const serverHttp = http.createServer(app)
 const io = new Server(serverHttp, {
 	allowEIO3: true,
 	cors: {
-		origin: ["https://master--luminous-cajeta-a0731b.netlify.app/"],
-		credentials: true,
+		origin: ["https://master--luminous-cajeta-a0731b.netlify.app"],
+		credentials: false,
 	},
 })
 
